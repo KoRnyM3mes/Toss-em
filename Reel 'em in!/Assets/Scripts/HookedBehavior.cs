7809e2ee1e6238bf7159d21babcc0ec399eead99
Christopher Tate <CrustRust15@gmail.com>
2025-02-27 09:42:13 -0700

    Attempt to build for android

--- Reel 'em in!/Assets/Scripts/HookedBehavior.cs
+++ Reel 'em in!/Assets/Scripts/HookedBehavior.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class HookedBehavior : MonoBehaviour
 {
-    public Transform lure; // The lure to hook onto
-    public float hookSpeed = 5f; // Speed at which the hooked object follows the lure
+    public float hookSpeed = 500f; // Speed at which the hooked object follows the lure
     public Vector3 offset; // Offset between the hooked object and the lure
-
+    private Transform lure; // The lure to hook onto
     private bool isHooked = false;
+
+    void Start()
+    {
+        // Hard-code the lure object by finding it in the scene (ensure you have a GameObject named "Lure")
+        lure = GameObject.Find("Lure").transform;
+    }
 
     void Update()
     {
@@ -21,6 +26,8 @@ public class HookedBehavior : MonoBehaviour
     // Call this method to hook the object to the lure
     public void HookToLure()
     {
+        // Assign random values to the x and z components of the offset while keeping the y component the same
+        offset = new Vector3(Random.Range(-1f, 1f), offset.y, Random.Range(-1f, 1f));
         isHooked = true;
     }
 
